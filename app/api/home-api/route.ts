5e6f7dcd7e172db5ad270921dc3344745114a448
Muhammad Habil Aswad <habil_asw@mhs.usk.ac.id>
2025-05-20 00:15:40 +0700

    fix: home-api

--- app/api/home-api/route.ts
+++ app/api/home-api/route.ts
@@ -1,3 +1,4 @@
+// app/api/home-api/route.ts
 import { NextResponse } from "next/server";
 import connectDB from "@/lib/mongodb";
 import Image from "../../models/Image"; // Model Image
@@ -5,20 +6,19 @@ import User from "../../models/User"; // Model User
 
 export async function GET(request: Request) {
   try {
-    await connectDB(); // Pastikan koneksi ke database berhasil
+    await connectDB();
 
-    // Melakukan aggregation untuk mendapatkan informasi gambar + pengguna berdasarkan email
     const hasilPencarian = await Image.aggregate([
       {
         $lookup: {
-          from: "users", // Koleksi Users
-          localField: "email", // Field di Image yang akan dicocokkan
-          foreignField: "email", // Field di User yang akan dicocokkan
-          as: "userInfo", // Nama alias hasil gabungan
+          from: "users",
+          localField: "email",
+          foreignField: "email",
+          as: "userInfo",
         },
       },
       {
-        $unwind: "$userInfo", // Mengurai hasil gabungan menjadi field yang lebih mudah diakses
+        $unwind: "$userInfo",
       },
       {
         $project: {
@@ -28,26 +28,27 @@ export async function GET(request: Request) {
           description: 1,
           likes: 1,
           email: 1,
-          "userInfo.linkedin": 1, // Menambahkan linkedin pengguna
-          "userInfo.github": 1, // Menambahkan github pengguna
-          "userInfo.tier": 1, // Menambahkan tier pengguna
-          "userInfo.points": 1, // Menambahkan point pengguna
+          "userInfo.linkedin": 1,
+          "userInfo.github": 1,
+          "userInfo.tier": 1,
+          "userInfo.points": 1,
         },
       },
     ]);
 
     console.log("Hasil gambar dan pengguna:", hasilPencarian);
 
-    // Mengirimkan hasil gambar dan informasi pengguna sebagai respons
     return new NextResponse(JSON.stringify(hasilPencarian), {
       headers: {
-        'Content-Type': 'application/json',
-        'Cache-Control': 'no-store', // Agar tidak cache
+        "Content-Type": "application/json",
+        "Cache-Control": "no-store", // penting agar tidak di-cache
       },
     });
-
   } catch (error) {
     console.error("Error fetching images and user data:", error);
-    return NextResponse.json({ error: "Failed to fetch images and user data" }, { status: 500 });
+    return NextResponse.json(
+      { error: "Failed to fetch images and user data" },
+      { status: 500 }
+    );
   }
 }
